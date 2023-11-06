2e929d616eedbcd86fc8f6a58848ef1ee8ca60cc
Pedro Dagostini <87324617+Dagostini01@users.noreply.github.com>
2023-11-06 14:27:40 -0300

    classe task

---
+++ src/main/java/br/com/cp2digital/model/Task.java
@@ -0,0 +1,16 @@
+package br.com.cp2digital.model;
+
+import java.util.Date;
+
+public class Task {
+    private Long id;
+    private String title;
+    private String description;
+    private Boolean status;
+    private Date dueDate;
+    private Boolean isActive;
+
+    public Task(){}
+}
+
+
