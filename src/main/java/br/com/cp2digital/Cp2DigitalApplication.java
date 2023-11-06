adb3a6b64680efb16016548411b4e4714c6b0e0a
Pedro Dagostini <87324617+Dagostini01@users.noreply.github.com>
2023-11-06 14:09:52 -0300

    Initial commit

---
+++ src/main/java/br/com/cp2digital/Cp2DigitalApplication.java
@@ -0,0 +1,13 @@
+package br.com.cp2digital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cp2DigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cp2DigitalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/cp2digital/Cp2DigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cp2digital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cp2DigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
